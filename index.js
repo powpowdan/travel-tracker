a2f4403dcd859d8d7b0487a9e2eb2590f72538ac
powpowdan <powpowdan@gmail.com>
2024-09-21 00:38:30 -0400

    part 2 complete

--- index.js
+++ index.js
@@ -17,24 +17,43 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {  
-  //get the country codes from our table
-  //store them in a countries variable, looping over each
-  //pass that data over to ejs
-const result = await db.query(
-  "Select country_code from visited_countries"
-);
+app.get("/", async (req, res) => {
+  const result = await db.query("Select country_code from visited_countries");
+  let visited_countries = [];
+  result.rows.forEach((country) => {
+    visited_countries.push(country.country_code);
+  });
+  // console.log(visited_countries);
 
-let visited_countries = [];
-result.rows.forEach((country) => {
-  visited_countries.push(country.country_code); 
+  let count = visited_countries.length;
+  res.render("index.ejs", { countries: visited_countries, total: count++ });
 });
-console.log(visited_countries);
-  
- let count = visited_countries.length;
-res.render('index.ejs', { countries: visited_countries, total: count++ });
- db.end();
 
+//create app.post (/add) just like .get
+// get user input
+//check that input against the countries list.
+//if it exists, get the country code
+//add the country code to visited_countries
+//refresh page
+app.post("/add", async (req, res) => {
+  let userInput = req.body.country;
+  //  console.log("user input = " + userInput);
+
+  const match = await db.query(
+    "SELECT * FROM countries WHERE country_name = $1",
+    [userInput]
+  );
+
+  let countryName = match.rows[0].country_name;
+  let countryCode = match.rows[0].country_code;
+  // console.log("just country code " + countryCode);
+  // console.log("just country name " + countryName); 
+  if (userInput === countryName) {
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
